31ce0a0c5c874fb21db343633a832d57f59becea
JuryOrava <98976778+JuryOrava@users.noreply.github.com>
2023-03-12 20:52:08 +0300

    Delete controllers/server-err-logs directory

--- controllers/server-err-logs/error-logs.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-module.exports.writeTextToFile = (filename, text) => {
-  fs.writeFile(filename, text + "\r\n", function (err, res) {
-      console.log(`fs.writeFile завершилась с таким результатом: ${res}`)
-  })
-}
