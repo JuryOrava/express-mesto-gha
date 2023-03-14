5b966cbe6cd879ca17336634a1dd7842f3c4b1e1
Jury Orava <jury.orava@yandex.ru>
2023-03-14 22:59:53 +0300

    commit

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,16 +3,20 @@ const { writeTextToFile } = require('../errors/server-err-logs/error-logs');
 
 const NotFoundError = require('../errors/not-found-err'); // 404
 const BadRequesrError = require('../errors/bad-request-err'); // 400
-const ClientError = require('../errors/client-err'); // 401
+// const ClientError = require('../errors/client-err'); // 401
 const InternalServerError = require('../errors/internal-server-err'); // 500
 
 const date = Date.now();
 const serverErrorFile = `../errors/server-err-logs/log-${date}.txt`;
 
 module.exports.createCard = (req, res, next) => {
-  const { name, link } = req.body;
+  const {
+    name, link, email, password,
+  } = req.body;
 
-  Card.create({ name, link, owner: req.user._id })
+  Card.create({
+    name, link, owner: req.user._id, email, password,
+  })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -36,26 +40,28 @@ module.exports.getCards = (req, res, next) => {
     .catch(next);
 };
 module.exports.deleteCard = (req, res, next) => {
-  if (req.user._id === req.params.owner._id) {
-    Card.findByIdAndRemove(req.params.cardId)
-      .then((card) => {
-        if (card == null) {
-          throw new NotFoundError(`Передан несуществующий _id:${req.params.cardId} карточки.`);
-        }
-        res.send({ data: card });
-      })
-      .catch((err) => {
-        if (err.name === 'CastError') {
-          throw new BadRequesrError(`Карточка с указанным _id:${req.params.cardId} не найдена.`);
-        } else {
+  /* if (req.user._id === req.params.owner._id) { */
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (card == null) {
+        throw new NotFoundError(`Передан несуществующий _id:${req.params.cardId} карточки.`);
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        throw new BadRequesrError(`Карточка с указанным _id:${req.params.cardId} не найдена.`);
+      } else {
+        // eslint-disable-next-line indent
           writeTextToFile(serverErrorFile, `Дата и время ошибки: ${new Date()}; Текст ошибки: ${err.message}`);
-          throw new InternalServerError('На сервере произошла ошибка.');
-        }
-      })
-      .catch(next);
-  } else {
-    throw new ClientError(`Это не Ваша карточка. Ай-яй-яй. Если она Вам не нравится, попросите пользователя: ${req.params.owner.id} удалить её =)`);
-  }
+        throw new InternalServerError('На сервере произошла ошибка.');
+      }
+    })
+    .catch(next);
+  /* } else {
+    throw new ClientError(`Это не Ваша карточка. Ай-яй-яй.
+    Если она Вам не нравится, попросите пользователя: ${req.params.owner.id} удалить её =)`);
+  } */
 };
 
 module.exports.likeCard = (req, res, next) => {
